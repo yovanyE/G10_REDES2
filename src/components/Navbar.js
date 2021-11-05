b4fae5435505fb93e0f152d8a9534ea1d0bad822
yovanyE <orozco77@>
2021-11-05 07:43:25 -0600

    navbar agregado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
-
+    const history = useHistory();
+    const redirigir = (path) => {
+        history.push(path);
+    }
     return (
         <Nav
             activeKey="/"
         >
             <Nav.Item>
-                <Nav.Link href="/">Lista de reportes</Nav.Link>
+                <Nav.Link onClick={() => redirigir("/")}>Lista de reportes</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/report">Crear Reporte</Nav.Link>
+                <Nav.Link onClick={() => redirigir("/report")} >Crear Reporte</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/evento/asistencia">Ingresar Asistencia</Nav.Link>
+                <Nav.Link onClick={() => redirigir("/evento/asistencia")}  >Ingresar Asistencia</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/evento/asistencias">Asistencias de Evento</Nav.Link>
+                <Nav.Link onClick={() => redirigir("/evento/asistencias")} >Asistencias de Evento</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/estudiante/eventos">Eventos estudiante</Nav.Link>
+                <Nav.Link onClick={() => redirigir("/estudiante/eventos")} >Eventos estudiante</Nav.Link>
             </Nav.Item>
         </Nav>
     )
